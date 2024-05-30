93fff3fe70dec01e48a98624ad2279e79e625b3c
Zuveriya Munshi <zuveriyamunshi924@gmail.com>
2024-05-30 12:37:35 +0530

    Day 6 LoginAPI

---
+++ Day 6 LoginAPI/Registration FrontEnd/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+// import { Routes } from '@angular/router';
+
+// export const routes: Routes = [];
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+// import { RegisterComponent } from './register/register.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
